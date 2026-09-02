b9309f112aad4d26891ec098a02ba0eaa5c1a7b7
Hardik <hardikmanchanda2007@gmail.com>
2026-09-02 15:00:55 +0530

    fix: resolve Render build 127 error by adding unified build script and static file serving

--- server/server.js
+++ server/server.js
@@ -76,6 +76,15 @@ app.get('/api/health', (req, res) => {
   });
 });
 
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  });
+}
+
 // Error Handler (must be last)
 app.use(errorHandler);
 
